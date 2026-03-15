827939812e515f6d3b6b282673f3aa644615ee33
Lewis Lane <lew@rotates.org>
2026-03-15 22:30:47 +0000

    Improved asset preloading.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-// Pre-load UI images so we don't get pop-in when they are first used
+// Pre-load UI images so we don't get pop-in when they are first used.
+// new Image().src triggers an immediate fetch (unlike link[rel=preload] which browsers may ignore).
 const uiImages = import.meta.glob("../assets/images/ui/**", { eager: true, import: "default" });
-Object.keys(uiImages).forEach((key) => {
-    const img = uiImages[key] as { default: string };
-    if (!key || !img?.default) {
-        return;
+Object.values(uiImages).forEach((src) => {
+    if (src) {
+        new Image().src = src as string;
     }
-    const link = document.createElement("link");
-    link.rel = "preload";
-    link.as = "image";
-    link.href = img.default;
-    document.head.appendChild(link);
 });
 
 createApp(App).mount("#app");
