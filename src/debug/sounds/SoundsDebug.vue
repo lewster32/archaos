49acd559a01a8813aead6b0a2f833bfed42b3e56
Lewis Lane <lew@rotates.org>
2026-04-11 15:15:00 +0100

    feat: add SoundsDebug panel with play buttons for all chaossounds entries

---
+++ src/debug/sounds/SoundsDebug.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <h1>Sounds</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>outerCount</th>
+                    <th>middleCount</th>
+                    <th>pitch</th>
+                    <th>increments</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="sound in sounds" :key="sound.id">
+                    <td>{{ sound.id }}</td>
+                    <td>{{ sound.outerCount }}</td>
+                    <td>{{ sound.middleCount }}</td>
+                    <td>{{ sound.pitch.join(", ") }}</td>
+                    <td>{{ sound.increments.join(", ") }}</td>
+                    <td>
+                        <button
+                            :disabled="playing"
+                            @click="play(sound)"
+                        >
+                            Play
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import soundsData from "../../../assets/data/chaossounds.json";
+import { playSound } from "../../gameobjects/chaossounds";
+import type { SoundParams } from "../../gameobjects/chaossounds";
+
+interface SoundEntry extends SoundParams {
+    id: string;
+    calledFrom: string[];
+}
+
+const sounds = soundsData as SoundEntry[];
+const playing = ref(false);
+
+async function play(sound: SoundEntry): Promise<void> {
+    playing.value = true;
+    try {
+        await playSound(sound);
+    } finally {
+        playing.value = false;
+    }
+}
+</script>
